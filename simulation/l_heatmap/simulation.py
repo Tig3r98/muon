6ab8c33e749fe15aed76617038e047ebb6ec33c1
Tig3r <fox.98@live.it>
2025-05-02 14:36:34 +0200

    first commit

---
+++ simulation/l_heatmap/simulation.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+import time
+
+# variabili di sistema
+n_event = 10_000_000
+dim_x = 0.8 #m
+dim_y = 0.3
+#dim_z = 0.02 ?
+bins_per_centimeter = 1
+n_processes = 24
+
+steps = 1
+sim_from = 3.0
+
+def sample_theta():
+    while True:
+        theta_toy = np.random.uniform(0, np.pi / 2)
+        y = np.random.uniform(0, 1)  # massimo di cos^2(theta) è 1
+        if y < np.cos(theta_toy)**2:
+            return theta_toy
+
+def sample_phi():
+    phi_toy = np.random.uniform(0, 2 * np.pi)
+    return phi_toy
+
+def simulate_events(args):
+    l, n = args  # unpack the tuple
+    print("[THREAD] Eseguendo simulazione con L="+str(l)+" su "+str(n)+" eventi")
+    #generiamo x_0 y_0 come punto di intersezione del muone sullo scintillatore 0, quindi z_0=0
+    coinc = 0
+    x_vals = []
+    y_vals = []
+    for i in range(n):
+        theta = sample_theta()
+        phi = sample_phi()
+        x_0 = np.random.uniform(0, dim_x)
+        y_0 = np.random.uniform(0, dim_y)
+
+        x_1 = x_0 + np.tan(theta) * l * np.cos(phi)
+        y_1 = y_0 + np.tan(theta) * l * np.sin(phi)
+
+        # controlla se il punto di intersezione sul secondo piano
+        if (0 < x_1 and x_1 < dim_x) and (0 < y_1 and y_1 < dim_y):
+            coinc += 1
+            x_vals.append(x_0)
+            y_vals.append(y_0)
+    return x_vals, y_vals, coinc
+
+def simulate(z_1):
+    
+    l = np.abs(z_1)
+    # Run parallel simulations
+    events_per_proc = n_event // n_processes
+    print("Eseguendo "+str(events_per_proc)+" eventi su "+str(n_processes)+" processori")
+    with ProcessPoolExecutor(max_workers=n_processes) as executor:
+        args_list = [(l, events_per_proc)] * n_processes
+        results = list(executor.map(simulate_events, args_list))
+    print("Simulazione terminata. Elaborazione risultati...")
+    
+    # Combine results
+    x_values = []
+    y_values = []
+    n_coinc = 0
+    
+    for x_vals, y_vals, coinc in results:
+        x_values.extend(x_vals)
+        y_values.extend(y_vals)
+        n_coinc += coinc
+    
+    perc = "0%"
+    if n_coinc == 0:
+        print("Nessuna coincidenza.")
+    else:
+        perc = str(round((n_coinc/n_event)*100, 2))+"%"
+        print("Coincidenze: "+str(n_coinc)+" su "+str(n_event)+" - "+perc)
+    
+    heatmap, xedges, yedges = np.histogram2d(x_values, y_values, bins=(round(dim_x*100)*bins_per_centimeter, round(dim_y*100)*bins_per_centimeter))
+    
+    # Plot the heatmap
+    plt.figure(figsize=(dim_x*10, dim_y*10+1))
+    plt.imshow(
+        heatmap.T,
+        extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+        origin='lower',
+        cmap='hot',
+        aspect='auto'
+    )
+    plt.colorbar(label='Density')
+    plt.title("L="+str(l)+", "+str(n_event//1000000)+"Mevents, "+perc+" Aeff")
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(False)
+    plt.savefig(str(l)+".png")
+    #plt.show()
+
+if __name__ == "__main__":
+    start = int(time.time())
+    print("Running "+str(steps)+" simulation steps from "+str(sim_from)+" with "+str(n_event)+" events per simulation")
+    for i in range(steps):
+        l = (i+1) * sim_from
+        simulate(l)
+    stop = int(time.time())
+    print("Took "+str(stop-start)+"s.")
